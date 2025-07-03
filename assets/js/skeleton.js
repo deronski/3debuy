812bd0d75df61ad795adf270efe6e236d4eba7b7
deronski <Derekokoro@yahoo.com>
2025-07-03 20:23:31 +0100

    push

---
+++ assets/js/skeleton.js
@@ -0,0 +1,21 @@
+var infoButtons = document.getElementsByClassName("annotation");
+var infoContainer = document.getElementById("infoContainer");
+var infoParagraphs = infoContainer.getElementsByTagName("p");
+
+// Add click event listeners to each button
+for (var i = 0; i < infoButtons.length; i++) {
+  infoButtons[i].addEventListener("click", function() {
+    infoContainer.classList.toggle("show");
+    var infoId = this.getAttribute("data-info");
+    
+
+    // Show the selected information and hide others
+    for (var j = 0; j < infoParagraphs.length; j++) {
+      infoParagraphs[j].classList.remove("active");
+    }
+    
+    var selectedInfo = document.getElementById(infoId);
+    selectedInfo.classList.add("active");
+    infoContainer.classList.add("show");
+  });
+}
